a6dfe28859e56385c3bf8c059aac075be785c4f8
White-Oak <lpzhelud@gmail.com>
2015-11-09 00:23:31 +0300

    Made a finally working solution using in-heap array

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
-#![feature(test)]
-extern crate test;
 extern crate time;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test::Bencher;
     use std::iter::*;
+    use time::*;
 
-    #[bench]
-    fn process_array(b: &mut Bencher) {
-        const W: i32 = 25600;
-        const H: i32 = 2048;
-        const SIZE: usize = (W * H) as usize;
-        let mut v: Vec<i32> = repeat(0).take(SIZE).collect();
-        b.iter(|| {
-            for i in 0..W {
-                for j in 0..H{
-                    v[(i + W * j) as usize] = i * j;
-                } 
-            }
-        });
-        println!("Element is {}", v[(W + 1) as usize]);
+    fn main() {
+    const W: i32 = 25600;
+    const H: i32 = 2048;
+    const SIZE: usize = (W * H) as usize;
+    let mut v: Vec<i32> = repeat(0).take(SIZE).collect();
+    let start = precise_time_ns();
+    for i in 0..W {
+        for j in 0..H{
+            v[(i + W * j) as usize] = i * j;
+        } 
     }
+    let total = precise_time_ns() - start;
+    println!("{}", total / 1000 / 1000);
 }
