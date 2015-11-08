981e781ef0f7e7a697fd0c72041e48815dde6076
White-Oak <lpzhelud@gmail.com>
2015-11-09 01:05:09 +0300

    Modified in-heap solution using write to /dev/null

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,26 @@
 extern crate time;
 
 use time::precise_time_ns;
-use std::thread;
-use std::mem::size_of;
-use std::io::*;
+use std::io::BufWriter;
+use std::io::prelude::*;
 use std::fs::OpenOptions;
 use std::path::Path;
+use std::iter::repeat;
 
 fn main() {
     const W: i32 = 25600;
     const H: i32 = 2048;
     const SIZE: usize = (W * H) as usize;
-    let child = thread::Builder::new()
-        .stack_size(SIZE * size_of::<i32>() * 2)
-        .spawn(move || {
-            let mut v: [i32; SIZE] = [0; SIZE];
-            let start = precise_time_ns();
-            for i in 0..W {
-                for j in 0..H{
-                    v[(i + W * j) as usize] = i * j;
-                } 
-            }
-            let total = precise_time_ns() - start;
-            println!("{}", total / 1000 / 1000);
-            write_to_null(v[(W+1) as usize]);
-        }).unwrap();
-    child.join();
+    let mut v: Vec<i32> = repeat(0).take(SIZE).collect();
+    let start = precise_time_ns();
+    for i in 0..W {
+        for j in 0..H{
+            v[(i + W * j) as usize] = i * j;
+        } 
+    }
+    let total = precise_time_ns() - start;
+    println!("{}", total / 1000 / 1000);
+    write_to_null(v[(W+1) as usize]);
 }
 
 fn write_to_null(i :i32) {
