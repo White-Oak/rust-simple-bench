2882c389f867bd3d150a7e3acc00793c7b65de63
White-Oak <lpzhelud@gmail.com>
2015-11-09 00:36:34 +0300

    Made a benchmark with an array on-stack
    Consider moving in-heap solution to an other braanch.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 extern crate time;
 
-    use std::iter::*;
-    use time::*;
+use time::precise_time_ns;
+use std::thread;
+use std::mem::size_of;
 
-    fn main() {
+fn main() {
     const W: i32 = 25600;
     const H: i32 = 2048;
     const SIZE: usize = (W * H) as usize;
-    let mut v: Vec<i32> = repeat(0).take(SIZE).collect();
-    let start = precise_time_ns();
-    for i in 0..W {
-        for j in 0..H{
-            v[(i + W * j) as usize] = i * j;
-        } 
-    }
-    let total = precise_time_ns() - start;
-    println!("{}", total / 1000 / 1000);
+    let child = thread::Builder::new()
+        .stack_size(SIZE * size_of::<i32>() * 2)
+        .spawn(move || {
+            let mut v: [i32; SIZE] = [0; SIZE];
+            let start = precise_time_ns();
+            for i in 0..W {
+                for j in 0..H{
+                    v[(i + W * j) as usize] = i * j;
+                } 
+            }
+            let total = precise_time_ns() - start;
+            println!("{}", total / 1000 / 1000);
+            println!("{}", v[(W+1) as usize]);
+        }).unwrap();
+    child.join();
 }
